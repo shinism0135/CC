df821e19019b37774925e3fee9c9f96ac4d2b8db
30910 <129143486+shinism0135@users.noreply.github.com>
2023-04-11 05:18:34 +0000

    홀수 짝수 추가

--- ifelse.c
+++ ifelse.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-main() {
-  int a = 5;
+// main() {
+//   int a = 5;
 
-  if (a > 3)   // if(참)
-    printf("A\n");  //조건문이 참이면 실행
-  else
-    printf("B\n");  //조건문이 거짓이면 실행
-  printf("C\n");
+//   if (a > 3)   // if(참)
+//     printf("A\n");  //조건문이 참이면 실행
+//   else
+//     printf("B\n");  //조건문이 거짓이면 실행
+//   printf("C\n");
 
-  if (a > 8) printf("D/n");  //조건문이 참이면 실행
+//   if (a > 8) printf("D/n");  //조건문이 참이면 실행
 
-  else printf("E\n");  //조건문이 거짓이면 실행
+//   else printf("E\n");  //조건문이 거짓이면 실행
 
-  printf("F\n");
-}
+//   printf("F\n");
+// }

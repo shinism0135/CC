fc68e0a98904262eda813f16ebc20a55a174aecb
shinism0135 <129143486+shinism0135@users.noreply.github.com>
2023-05-23 05:06:29 +0000

    while1

--- .ccls-cache/@home@runner@Cbal/while.c
+++ .ccls-cache/@home@runner@Cbal/while.c
@@ -1,12 +1,12 @@
-// main(){
-//   int i;
-//   int sum=0;
+main(){
+  int i;
+  int sum=0;
 
-//   i=1;
+  i=1;
 
-//   while(i<=2019){
-//     sum+= i;
-//     i++;
-//   }
-//   printf("1에서 2019까지의 합은 %d\n", sum);
-// }
+  while(i<=2019){
+    sum+= i;
+    i++;
+  }
+  printf("1에서 2019까지의 합은 %d\n", sum);
+}
